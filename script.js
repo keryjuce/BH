d2a8c7b5a678345045a15fab3eee355e4cc03047
keryjuce <151083420+keryjuce@users.noreply.github.com>
2024-02-11 19:07:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,19 +30,3 @@ if(info2 && infoOpen2) {
         bodySec.classList.remove('lock')
     })
 }
-
-const info3 = document.getElementById("cources_active_item_3")
-const infoOpen3 = document.getElementById("button_open_3")
-const closeBtn3 = document.getElementById("button_close_3")
-
-
-if(info3 && infoOpen3) {
-    infoOpen3.addEventListener('click', () =>{
-        info3.classList.add('shown')
-        bodySec.classList.add('lock')
-    })
-    closeBtn3.addEventListener('click', () =>{
-        info3.classList.remove('shown')
-        bodySec.classList.remove('lock')
-    })
-}
